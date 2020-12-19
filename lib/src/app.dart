fc163e5a62ad70f41b7c5607765dff48d614a483
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-12-19 00:35:48 -0500

    07 Widget Scaffold

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,9 @@
+import 'package:contador/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text('Hola Mundo')));
+    return MaterialApp(home: Center(child: HomePage()));
   }
 }
