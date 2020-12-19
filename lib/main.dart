4d377ef0d0b7d0e184c81cf3a6ba5115ab74c6e3
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-12-19 00:26:55 -0500

    06 Login folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
+import 'package:contador/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text('Hola Mundo')));
-  }
-}
